5790025cd55651e839b49482e026a49470e7ac13
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-06-05 13:39:11 +0200

    "Updated App.jsx: reformatted code, changed import paths, and modified navigation links"

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import { BrowserRouter , Routes, Route, Link} from 'react-router-dom';
-import About from './About';
-import Home from './page/Home';
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import About from "./page/About";
+import Home from "./page/Home";
 
-
-export default function App(){
+export default function App() {
   return (
-  <BrowserRouter>
-  <header>
-  <nav>
-  <Link t0="/home">#VANLive</Link>
-  <Link t0="/about">About</Link>
-  </nav>
-  </header>
-  
-  <Routes>
-     <Route path='/' element={<Home />} />
-     <Route path='/about' element={<About />} />
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">
+          #VANLive
+        </Link>
+        <nav>
+          <Link t0="/about">About</Link>
+        </nav>
+      </header>
 
-    </Routes>
-  </BrowserRouter>
-  )
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-
