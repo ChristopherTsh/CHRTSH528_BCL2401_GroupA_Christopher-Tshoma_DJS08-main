15eef18a77926fff1e0a2b8f37afe26649360cbf
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-06-05 13:29:45 +0200

    "Refactored App component to import Home and About pages, updated navbar and routes"

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,18 @@
 import React from 'react';
 import { BrowserRouter , Routes, Route, Link} from 'react-router-dom';
-/**
- * Challenge:
- * Bootstrap the VanLife project by creating the first 2 routes:
- * Home and About.
- * 
- * Also include the navbar that can link between the two routes.
- * For now, you'll either need to copy/paste the navbar code
- * to both Home and About pages, or you'll need to find a place
- * to put it where it can be shared between the two pages.
- * (Don't overthink this part - just do whatever is easiest for
- * you because we'll learn a better approach very soon)
- * 
- * Review challenge: do all the CSS yourself based on the design
- * linked in the slides.
- */
+import About from './About';
+import Home from './page/Home';
 
 
-export function App(){
+export default function App(){
   return (
   <BrowserRouter>
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <Link t0="/">Home</Link>
+  <header>
+  <nav>
+  <Link t0="/home">#VANLive</Link>
   <Link t0="/about">About</Link>
   </nav>
+  </header>
   
   <Routes>
      <Route path='/' element={<Home />} />
@@ -34,13 +23,4 @@ export function App(){
   )
 }
 
-function Home(){
-  return (
-    <h1>Hello, React Router!</h1>
-  )
-}
-function About(){
-  return (
-    <h1>About page goes here! 🎉</h1>
-  )
-}
+
