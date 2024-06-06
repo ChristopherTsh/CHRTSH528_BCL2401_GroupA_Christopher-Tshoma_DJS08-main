df7662367f1bf09dddf33a93130c86ed1ddb6a0b
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-06-06 15:24:12 +0200

    "Refactor App routing and layout, add HostLayout component, and update Header and Dashboard components"

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 import About from "./page/About";
 import Home from "./page/Home";
 import Vans from "./page/Vans";
@@ -8,6 +8,7 @@ import Dashboard from "./Host/dashboard";
 import Income from "./Host/Income";
 import Reviews from "./Host/Reviews";
 import Layout from "./components/Layout";
+import HostLayout from "./components/HostLayout";
 
 import './server'
 
@@ -15,14 +16,18 @@ export default function App() {
   return (
     <BrowserRouter>
         <Routes>
-        <Route element={<Layout />} >
-          <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout />} >
+          <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<Vans />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
-          <Route path="/host" element={<Dashboard />} />
-          <Route path="/host/income" element={<Income />} />
-          <Route path="/host/reviews" element={<Reviews />} />
+          <Route path="/vans" element={<Vans />} >
+            <Route path="/vans/:id" element={<VanDetail />} />
+          </Route>
+
+          <Route path="/host" element={<HostLayout />} >
+            <Route index element={<Dashboard />} />
+            <Route path="/host/income" element={<Income />} />
+            <Route path="/host/reviews" element={<Reviews />} />
+          </Route>
         </Route>
       </Routes>
     </BrowserRouter>
