1a169285e8be19b499c528503289485642c78ea8
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-06-06 10:36:55 +0200

    "Removed header and navigation elements from App component"

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,13 @@ import About from "./page/About";
 import Home from "./page/Home";
 import Vans from "./page/Vans";
 import VanDetail from "./page/VanDetail"
+
 import './server'
 
 export default function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">
-          #VANLive
-        </Link>
-        <nav>
-          <Link to="/About">About</Link>
-          <Link to="/Vans">Vans</Link>
-        </nav>
-      </header>
-
-      <Routes>
+        <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans />} />
