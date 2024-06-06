b5f7dceacbca6de52fc40a37109b2b44554c5d4d
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-06-06 11:13:48 +0200

    "Refactor App routing and add Header component to Layout"

--- src/App.jsx
+++ src/App.jsx
@@ -12,11 +12,12 @@ export default function App() {
   return (
     <BrowserRouter>
         <Routes>
-        <Route element={<Layout />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<Vans />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />} >
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<Vans />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
