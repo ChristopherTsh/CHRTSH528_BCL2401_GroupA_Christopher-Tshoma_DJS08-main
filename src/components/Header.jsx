d8e950a44f15f5e89da38920a35874f58f1a2010
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-06-06 21:48:20 +0200

    "Updated Header component with new styles, added fakeLogOut function, and login link with avatar icon"

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,44 @@
-import React from "react"
-import { Link, NavLink } from "react-router-dom"
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
 export default function Header() {
-    return (
-        <header>
-            <Link className="site-logo" to="/">#VanLife</Link>
-            <nav>
-            <NavLink to="/host" className={({isActive}) => isActive ? "active-link" : null}>Host</NavLink>
-                <NavLink to="/about" className={({isActive}) => isActive ? "active-link" : null}>About</NavLink>
-                <NavLink to="/vans" className={({isActive}) => isActive ? "active-link" : null}>Vans</NavLink>
-            </nav>
-        </header>
-    )
+  const activeStyles = {
+    fontWeight: "bold",
+    textDecoration: "underline",
+    color: "#161616",
+  };
+
+  function fakeLogOut() {
+    localStorage.removeItem("loggedin");
+  }
+  return (
+    <header>
+      <Link className="site-logo" to="/">
+        #VanLife
+      </Link>
+      <nav>
+        <NavLink
+          to="/host"
+          className={({ isActive }) => (isActive ? "active-link" : null)}
+        >
+          Host
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={({ isActive }) => (isActive ? "active-link" : null)}
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/vans"
+          className={({ isActive }) => (isActive ? "active-link" : null)}
+        >
+          Vans
+        </NavLink>
+        <Link to="login" className="login-link">
+          <img src="/assets/images/avatar-icon.png" className="login-icon" />
+        </Link>
+      </nav>
+    </header>
+  );
 }
